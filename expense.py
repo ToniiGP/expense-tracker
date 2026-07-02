3ba0d4ad884c092acb8fdd3beb6a18ceb8bc0539
Juan Mejorado <juan.tony1516@gmail.com>
2026-07-02 18:58:33 -0500

    Initial project set up

---
+++ expense.py
@@ -0,0 +1,11 @@
+class Expense: 
+    def __init__(self, expense_id, amount, category, description, date): 
+        self.id = expense_id
+        self.amount = amount 
+        self.category = category 
+        self.description = description
+        self.date = date 
+        
+        
+        
+        
---
+++ main.py
@@ -0,0 +1,65 @@
+from expense import Expense
+
+CATEGORIES = [
+    "Food",
+    "Transportation",
+    "Entertainment",
+    "Bills",
+    "Shopping",
+    "Health",
+    "Other"
+]
+
+expenses = []
+
+def add_expense(expenses): 
+    expense_id = len(expenses) + 1
+    
+    amount = float(input("Enter the expense amount: "))
+    
+    while True: 
+        category = input("Enter the expense category: ")
+        if category in CATEGORIES:
+            break
+        else: 
+            print("That's not a valid category please try again!")
+            
+    description = input("Enter the expense description: ")
+    date = input("Enter the expense date: ")
+    
+    expense = Expense(expense_id, amount, category, description, date)
+    expenses.append(expense)
+    
+    print("Expense added succesfully!")
+            
+    
+    
+
+def display_menu(): 
+    
+    print("\nExpenses Tracker")
+    print("1. Add expense")
+    print("2. View expenses")
+    print("3. Exit")
+
+
+def main(): 
+    
+    while True: 
+        display_menu()
+        choice = input("Please choose an option: ")
+        
+        if choice == "1": 
+            add_expense(expenses)
+        elif choice == "2": 
+            print("View expenses selected")
+        elif choice == "3": 
+            print("Goodbye!!")
+            break
+        else: 
+            print("Invalid option, please try again!") 
+
+
+
+if __name__ == "__main__": 
+    main()
